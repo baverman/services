3bd3e1af0d6af5928887f5817e16e466113edce9
Anton Bobrov <bobrov@vl.ru>
2017-10-14 17:12:16 +0300

    initial

---
+++ haproxy/app.py
@@ -0,0 +1,12 @@
+import uwsgi
+
+def application(env, start_response):
+    if env['REQUEST_METHOD'] == 'OPTIONS':
+        content = b'Ok'
+    else:
+        # uwsgi.async_sleep(1)
+        content = b"Hello World"
+
+    start_response('200 OK', [('Content-Type', 'text/plain'),
+                              ('Content-Length', str(len(content)))])
+    return [content]
