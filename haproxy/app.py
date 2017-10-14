9f339c8d17f546329779603065be495876a7a5fb
Anton Bobrov <bobrov@vl.ru>
2017-10-14 17:24:37 +0300

    haproxy: uncomment sleep

--- haproxy/app.py
+++ haproxy/app.py
@@ -4,7 +4,7 @@ def application(env, start_response):
     if env['REQUEST_METHOD'] == 'OPTIONS':
         content = b'Ok'
     else:
-        # uwsgi.async_sleep(1)
+        uwsgi.async_sleep(1)
         content = b"Hello World"
 
     start_response('200 OK', [('Content-Type', 'text/plain'),
